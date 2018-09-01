f8acae40eabba7cfc6db9fbdad6c1b4290fd90c2
Rishi Gupta <gupt21@gmail.com>
2018-09-01 22:17:31 +0530

    add method to get java tmp directory

--- sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
+++ sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
@@ -4,8 +4,12 @@
 
 package flash.stm32.uart.internal;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 public final class SystemProperties {
 	
+	private String jtmpdir;
     private final SecurityManager securityManager;
 	
     /** 
@@ -15,4 +19,26 @@ public final class SystemProperties {
         securityManager = System.getSecurityManager();
     }
 
+    /** <p>Gives system/user temp directory as returned by JVM.</p>
+     * 
+     * @return tmp directory for Java operations.
+     * @throws SecurityException if security manager does not allow access to system property.
+     */
+    public String getJavaIOTmpDir() throws SecurityException {
+
+        if(this.jtmpdir != null) {
+            return jtmpdir;
+        }
+
+        if(securityManager == null) {
+            return System.getProperty("java.io.tmpdir");
+        }
+        else {
+            return (String) AccessController.doPrivileged(new PrivilegedAction<String>() {
+                public String run() {
+                    return System.getProperty("java.io.tmpdir");
+                }
+            });
+        }
+    }
 }
