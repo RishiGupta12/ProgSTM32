33c406bb6d7bc4779b0a5611ec44987fa7d44cf9
Rishi Gupta <gupt21@gmail.com>
2018-09-01 22:13:05 +0530

    get security manager instance to get system properties

--- sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
+++ sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
@@ -5,5 +5,14 @@
 package flash.stm32.uart.internal;
 
 public final class SystemProperties {
+	
+    private final SecurityManager securityManager;
+	
+    /** 
+     * <p>Allocates a new SystemProperties object.</p>
+     */
+    public SystemProperties() {
+        securityManager = System.getSecurityManager();
+    }
 
 }
