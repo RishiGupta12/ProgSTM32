fe5614e4531e85b9f158bf705879f586cd7f6aae
Rishi Gupta <gupt21@gmail.com>
2018-09-02 16:16:32 +0530

    add go command const define

--- sdk/uart/src/flash/stm32/uart/BLCMDS.java
+++ sdk/uart/src/flash/stm32/uart/BLCMDS.java
@@ -17,27 +17,36 @@ public final class BLCMDS {
 
     /**
      * <p>
-     * This command corresponds to Get Version & Read Protection Status command
-     * (0x01) of default STM32 Bootloader. This command gets the bootloader version
-     * and the read protection status of the flash memory
+     * This command corresponds to 'get version & read protection status' command
+     * (0x01) of default STM32 bootloader. This command gets the bootloader version
+     * and the read protection status of the flash memory.
      * </p>
      */
     public static final int GET_VRPS = 0x02;
-    
+
     /**
      * <p>
-     * This command corresponds to Get ID (0x02) command of default STM32 Bootloader. 
-     * This command gets the chip ID.
+     * This command corresponds to 'get ID' (0x02) command of default STM32
+     * bootloader. This command gets the chip ID.
      * </p>
      */
     public static final int GET_ID = 0x04;
-    
+
     /**
      * <p>
-     * This command corresponds to Read memory (0x11) command of default STM32 Bootloader. 
-     * This command reads up to 256 bytes of memory starting from an address specified by 
-     * the application.
+     * This command corresponds to 'read memory' (0x11) command of default STM32
+     * bootloader. This command reads up to 256 bytes of memory starting from an
+     * address specified by the application.
      * </p>
      */
     public static final int READ_MEMORY = 0x08;
+
+    /**
+     * <p>
+     * This command corresponds to 'go' (0x21) command of default STM32 bootloader.
+     * This command make jump to user application code located in the internal flash
+     * memory or in SRAM.
+     * </p>
+     */
+    public static final int GO = 0x08;
 }
