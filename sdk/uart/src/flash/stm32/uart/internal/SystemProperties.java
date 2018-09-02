47a143ccd551a2d94a2ea7c341e35cdfc29bb0f9
Rishi Gupta <gupt21@gmail.com>
2018-09-02 14:54:39 +0530

    replace tab by spaces for better formatting

--- sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
+++ sdk/uart/src/flash/stm32/uart/internal/SystemProperties.java
@@ -8,32 +8,33 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 
 public final class SystemProperties {
-	
-	private String jtmpdir;
+
+    private String jtmpdir;
     private final SecurityManager securityManager;
-	
-    /** 
+
+    /**
      * <p>Allocates a new SystemProperties object.</p>
      */
     public SystemProperties() {
         securityManager = System.getSecurityManager();
     }
 
-    /** <p>Gives system/user temp directory as returned by JVM.</p>
+    /**
+     * <p>Gives system/user temp directory as returned by JVM.</p>
      * 
      * @return tmp directory for Java operations.
-     * @throws SecurityException if security manager does not allow access to system property.
+     * @throws SecurityException
+     *             if security manager does not allow access to system property.
      */
     public String getJavaIOTmpDir() throws SecurityException {
 
-        if(this.jtmpdir != null) {
+        if (this.jtmpdir != null) {
             return jtmpdir;
         }
 
-        if(securityManager == null) {
+        if (securityManager == null) {
             return System.getProperty("java.io.tmpdir");
-        }
-        else {
+        } else {
             return (String) AccessController.doPrivileged(new PrivilegedAction<String>() {
                 public String run() {
                     return System.getProperty("java.io.tmpdir");
