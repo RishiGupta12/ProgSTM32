21dc42107c9b2f2834c8421cedb796cd59700f26
Rishi Gupta <gupt21@gmail.com>
2018-09-02 15:52:35 +0530

    add GET command const define

--- sdk/uart/src/flash/stm32/uart/BLCMDS.java
+++ sdk/uart/src/flash/stm32/uart/BLCMDS.java
@@ -5,5 +5,9 @@
 package flash.stm32.uart;
 
 public final class BLCMDS {
+    
+    /**<p>STM32 bootloader GET command (0x00). This command gets the version and 
+     * the allowed commands supported by the current version of the bootloader.</p>*/
+    public static final int GET = 0x01;
 
 }
