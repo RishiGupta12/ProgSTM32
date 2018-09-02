d6fc446f7c258cc965bfecfa5b39f0a0a85fb9e6
Rishi Gupta <gupt21@gmail.com>
2018-09-02 15:56:20 +0530

    add getting read protection status command const

--- sdk/uart/src/flash/stm32/uart/BLCMDS.java
+++ sdk/uart/src/flash/stm32/uart/BLCMDS.java
@@ -5,9 +5,23 @@
 package flash.stm32.uart;
 
 public final class BLCMDS {
-    
-    /**<p>STM32 bootloader GET command (0x00). This command gets the version and 
-     * the allowed commands supported by the current version of the bootloader.</p>*/
+
+    /**
+     * <p>
+     * This command corresponds to GET command (0x00) of default STM32 Bootloader.
+     * This command gets the version and the allowed commands supported by the
+     * current version of the bootloader.
+     * </p>
+     */
     public static final int GET = 0x01;
+
+    /**
+     * <p>
+     * This command corresponds to Get Version & Read Protection Status command
+     * (0x01) of default STM32 Bootloader. This command gets the bootloader version
+     * and the read protection status of the flash memory
+     * </p>
+     */
+    public static final int GET_VRPS = 0x02;
 
 }
