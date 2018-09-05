2eb0e7194c9ba033226757e866c3f92a93dc8bdf
Rishi Gupta <gupt21@gmail.com>
2018-09-05 08:21:59 +0530

    add class to categories flash memory regions

---
+++ sdk/uart/src/flash/stm32/uart/MEMTYPE.java
@@ -0,0 +1,38 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.uart;
+
+/**
+ * <p>
+ * Contains bit masks to categories flash memory area.
+ * </p>
+ * 
+ * @author Rishi Gupta
+ */
+public final class MEMTYPE {
+
+    /**
+     * <p>
+     * Represents main memory region in flash address space of the given microcontroller.
+     * </p>
+     */
+    public static final int MAIN = 0x01;
+
+    /**
+     * <p>
+     * Represents system memory region in information block in the flash address space of the 
+     * given microcontroller.
+     * </p>
+     */
+    public static final int SYSTEM = 0x02;
+
+    /**
+     * <p>
+     * Represents option byte memory region in information block in the flash address space of 
+     * the given microcontroller.
+     * </p>
+     */
+    public static final int OPTIONBYTE = 0x04;
+}
