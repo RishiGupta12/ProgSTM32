7145c2011e66b6977e9ad4bc5d75999e0714b653
Rishi Gupta <gupt21@gmail.com>
2018-09-08 21:20:25 +0530

    print info in colors if supported

--- scripts/colorchk.sh
+++ scripts/colorchk.sh
@@ -6,13 +6,13 @@
 function chk_termcolor_support {
 	command -v tput >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
-		echo "define prntylw" >> $1
-		echo "	@tput setaf 3" >> $1
+		echo "define pry" >> $1
+		echo "	@tput setaf 11" >> $1
 		echo "	@echo \$1" >> $1
 		echo "	@tput sgr0" >> $1
 		echo "endef" >> $1
 	else
-		echo "define prntylw" >> $1
+		echo "define pry" >> $1
 		echo "	@echo \$1" >> $1
 		echo "endef" >> $1
 	fi
