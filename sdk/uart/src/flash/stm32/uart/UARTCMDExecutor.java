9e0d6591fa06fac730482ec4eb47bc7e38908a02
Rishi Gupta <gupt21@gmail.com>
2018-09-09 11:10:30 +0530

    update read unprotect to use new sendCmdOrCmdData

--- sdk/uart/src/flash/stm32/uart/UARTCMDExecutor.java
+++ sdk/uart/src/flash/stm32/uart/UARTCMDExecutor.java
@@ -790,7 +790,7 @@ public final class UARTCMDExecutor {
         int res;
         byte[] buf = new byte[2];
         
-        res = sendCommand(CMD_READOUT_UNPROTECT);
+        res = sendCmdOrCmdData(CMD_READOUT_UNPROTECT);
         if (res == -1) {
             return 0;
         }
