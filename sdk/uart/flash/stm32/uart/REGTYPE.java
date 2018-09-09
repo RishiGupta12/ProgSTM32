1c2d7b52ff53fc16b649db1a7b977f920d275985
Rishi Gupta <gupt21@gmail.com>
2018-09-09 18:47:26 +0530

    restructure sdk build process

---
+++ sdk/uart/flash/stm32/uart/REGTYPE.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.uart;
+
+/**
+ * <p>
+ * Contains bit masks to categories flash memory area.
+ * </p>
+ * 
+ * @author Rishi Gupta
+ */
+public final class REGTYPE {
+
+    /**
+     * <p>
+     * Represents main memory region in flash address space of the given microcontroller.
+     * </p>
+     */
+    public static final int MAIN = 0x01;
+
+    /**
+     * <p>
+     * Represents system memory region in information block in the flash address space of the 
+     * given microcontroller.
+     * </p>
+     */
+    public static final int SYSTEM = 0x02;
+
+    /**
+     * <p>
+     * Represents option byte memory region in information block in the flash address space of 
+     * the given microcontroller.
+     * </p>
+     */
+    public static final int OPTIONBYTE = 0x04;
+    
+    /**
+     * <p>
+     * Represents bank 1 memory region in the flash address space of the given microcontroller.
+     * </p>
+     */
+    public static final int BANK1 = 0x08;
+    
+    /**
+     * <p>
+     * Represents bank 2 memory region in the flash address space of the given microcontroller.
+     * </p>
+     */
+    public static final int BANK2 = 0x10;
+}
---
+++ sdk/uart/flash/stm32/uart/package-info.java
@@ -0,0 +1,11 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+/**
+ * <p>Encapsulates behavior and environment for communication with 
+ * bootloader in STM32 microcontroller using UART.</p>
+ * 
+ * @author Rishi Gupta
+ */
+package flash.stm32.uart;
