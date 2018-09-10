767a4180410b43f326348287969d42ead2b50b51
Rishi Gupta <gupt21@gmail.com>
2018-09-10 19:38:48 +0530

    add devicemanager class as entry point to sdk

---
+++ sdk/core/flash/stm32/core/DeviceManager.java
@@ -0,0 +1,17 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core;
+
+/**
+ * <p>
+ * Entry point to the flashstm32 sdk. Application must obtain 
+ * an instance of this class and call methods in it.
+ * </p>
+ * 
+ * @author Rishi Gupta
+ */
+public class DeviceManager {
+
+}
