5ed28e9413534f39160ee758ab9b2b6dbcea5997
Rishi Gupta <gupt21@gmail.com>
2018-09-11 06:25:07 +0530

    make uart interface final class

--- sdk/uart/flash/stm32/uart/UARTInterface.java
+++ sdk/uart/flash/stm32/uart/UARTInterface.java
@@ -14,6 +14,6 @@ import flash.stm32.core.CommunicationInterface;
  * 
  * @author Rishi Gupta
  */
-public class UARTInterface extends CommunicationInterface {
+public final class UARTInterface extends CommunicationInterface {
 
 }
