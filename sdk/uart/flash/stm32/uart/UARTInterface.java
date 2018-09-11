c0c5ef3afb6da93a9d22373a1fbee78dee3fd250
Rishi Gupta <gupt21@gmail.com>
2018-09-11 06:40:31 +0530

    add implement method to open serial port

--- sdk/uart/flash/stm32/uart/UARTInterface.java
+++ sdk/uart/flash/stm32/uart/UARTInterface.java
@@ -41,6 +41,16 @@ public final class UARTInterface extends CommunicationInterface {
             SerialComManager.PARITY parity, SerialComManager.FLOWCONTROL flowCtrl)
             throws SerialComException {
         
+        comPortHandle = scm.openComPort(port, true, true, true);
+
+        scm.configureComPortData(comPortHandle, dataBits, stopBits, parity, baudRate, 0);
+
+        scm.configureComPortControl(comPortHandle, flowCtrl, 'x', 'x', false, false);
+
+        // 500 milliseconds timeout on serial port read
+        scm.fineTuneReadBehaviour(comPortHandle, 0, 5, 100, 5, 200);
+        
+        scm.clearPortIOBuffers(comPortHandle, true, true);
     }
 
 }
