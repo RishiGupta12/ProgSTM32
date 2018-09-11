d1d4be8a62717ac8c4db6f77cfc145e9f52a3de8
Rishi Gupta <gupt21@gmail.com>
2018-09-11 07:33:05 +0530

    add go cmds const

--- sdk/uart/flash/stm32/uart/internal/UARTCommandExecutor.java
+++ sdk/uart/flash/stm32/uart/internal/UARTCommandExecutor.java
@@ -28,6 +28,7 @@ public class UARTCommandExecutor extends CommandExecutor {
     private final byte[] CMD_GET_VRPS = new byte[] { (byte)0x01, (byte)0xFE };
     private final byte[] CMD_GET_ID = new byte[] { (byte)0x02, (byte)0xFD };
     private final byte[] CMD_READ_MEMORY = new byte[] { (byte)0x11, (byte)0xEE };
+    private final byte[] CMD_GO = new byte[] { (byte)0x21, (byte)0xDE };
     
     private final SerialComManager scm;
     private long comPortHandle;
