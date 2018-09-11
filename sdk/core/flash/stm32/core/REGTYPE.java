7b2e7144dcb83b5eba53cc8644ac767310926bdc
Rishi Gupta <gupt21@gmail.com>
2018-09-11 06:10:11 +0530

    update source formatting

--- sdk/core/flash/stm32/core/REGTYPE.java
+++ sdk/core/flash/stm32/core/REGTYPE.java
@@ -6,9 +6,8 @@ package flash.stm32.core;
 
 /**
  * <p>
- * Contains bit masks to categories flash memory area into 
- * main flash memory, system memory, option bytes and banks 
- * as applicable to given microcontroller.
+ * Contains bit masks to categories flash memory area into main flash memory,
+ * system memory, option bytes and banks as applicable to given microcontroller.
  * </p>
  * 
  * @author Rishi Gupta
@@ -17,37 +16,40 @@ public final class REGTYPE {
 
     /**
      * <p>
-     * Represents main memory region in flash address space of the given microcontroller.
+     * Represents main memory region in flash address space of the given
+     * microcontroller.
      * </p>
      */
     public static final int MAIN = 0x01;
 
     /**
      * <p>
-     * Represents system memory region in information block in the flash address space of the 
-     * given microcontroller.
+     * Represents system memory region in information block in the flash address
+     * space of the given microcontroller.
      * </p>
      */
     public static final int SYSTEM = 0x02;
 
     /**
      * <p>
-     * Represents option byte memory region in information block in the flash address space of 
-     * the given microcontroller.
+     * Represents option byte memory region in information block in the flash
+     * address space of the given microcontroller.
      * </p>
      */
     public static final int OPTIONBYTE = 0x04;
-    
+
     /**
      * <p>
-     * Represents bank 1 memory region in the flash address space of the given microcontroller.
+     * Represents bank 1 memory region in the flash address space of the given
+     * microcontroller.
      * </p>
      */
     public static final int BANK1 = 0x08;
-    
+
     /**
      * <p>
-     * Represents bank 2 memory region in the flash address space of the given microcontroller.
+     * Represents bank 2 memory region in the flash address space of the given
+     * microcontroller.
      * </p>
      */
     public static final int BANK2 = 0x10;
