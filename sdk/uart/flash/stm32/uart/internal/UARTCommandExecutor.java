d20ae17d95ad3262c4562c4072c694137451f9d8
Rishi Gupta <gupt21@gmail.com>
2018-09-11 07:34:17 +0530

    add write protect cmds const

--- sdk/uart/flash/stm32/uart/internal/UARTCommandExecutor.java
+++ sdk/uart/flash/stm32/uart/internal/UARTCommandExecutor.java
@@ -32,6 +32,7 @@ public class UARTCommandExecutor extends CommandExecutor {
     private final byte[] CMD_WRITE_MEMORY = new byte[] { (byte)0x31, (byte)0xCE };
     private final byte[] CMD_ERASE = new byte[] { (byte)0x43, (byte)0xBC };
     private final byte[] CMD_EXTD_ERASE = new byte[] { (byte)0x44, (byte)0xBB };
+    private final byte[] CMD_WRITE_PROTECT = new byte[] { (byte)0x63, (byte)0x9C };
     
     private final SerialComManager scm;
     private long comPortHandle;
