0c864fc56b6feec913d02cc4908a88bff27e3446
Rishi Gupta <gupt21@gmail.com>
2018-09-11 06:10:45 +0530

    update source formatting

--- sdk/uart/flash/stm32/uart/UARTInterface.java
+++ sdk/uart/flash/stm32/uart/UARTInterface.java
@@ -2,15 +2,14 @@
  * Copyright (C) 2018, Rishi Gupta. All rights reserved.
  */
 
-package flash.stm32.core;
+package flash.stm32.uart;
 
 import flash.stm32.core.CommunicationInterface;
 
 /**
  * <p>
- * Represents a serial port (UART interface) through which
- * host computer will communicate with bootloader in a stm32
- * device.
+ * Represents a serial port (UART interface) through which host computer will
+ * communicate with bootloader in a stm32 device.
  * </p>
  * 
  * @author Rishi Gupta
