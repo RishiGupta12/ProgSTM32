ae7c66a8e12b10712fc2c229e9a7d4aed33536f6
Rishi Gupta <gupt21@gmail.com>
2018-09-13 16:55:31 +0530

    add F0 x445 series dev

---
+++ sdk/core/flash/stm32/core/internal/DevF0x445.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF0x445 extends Device {
+
+    public DevF0x445() {
+        pid = 0x445;
+        mcuSeries = "STM32F04xxx - STM32F070x6";
+        RAMMemStartAddr = 0x00;
+        RAMMemEndAddr = 0x00;
+        IBSysMemStartAddr = 0x1FFFC400;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
