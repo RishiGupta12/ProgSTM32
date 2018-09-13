2a16396147158899efc2d53073d5d2c356a43787
Rishi Gupta <gupt21@gmail.com>
2018-09-13 16:21:10 +0530

    add L1 0x427 device

---
+++ sdk/core/flash/stm32/core/internal/DevL1x427.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL1x427 extends Device {
+
+    public DevL1x427() {
+        pid = 0x427;
+        mcuSeries = "STM32L1xxxC";
+        RAMMemStartAddr = 0x20001000;
+        RAMMemEndAddr = 0x20007FFF;
+        IBSysMemStartAddr = 0x1FF00000;
+        IBSysMemEndAddr = 0x1FF01FFF;
+    }
+}
