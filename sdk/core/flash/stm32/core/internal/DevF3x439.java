18ec08ee861b276e85a4ec53e31da194bc0e935a
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:29:52 +0530

    add f3 0x439 device

---
+++ sdk/core/flash/stm32/core/internal/DevF3x439.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF3x439 extends Device {
+
+    public DevF3x439() {
+        pid = 0x439;
+        mcuSeries = "STM32F301xx/302x4(6/8) - STM32F318xx";
+        RAMMemStartAddr = 0x20001800;
+        RAMMemEndAddr = 0x20003FFF;
+        IBSysMemStartAddr = 0x1FFFD800;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
