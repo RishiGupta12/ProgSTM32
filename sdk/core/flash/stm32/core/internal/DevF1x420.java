6074fde2de6b040e6b390901c42bb040ee4a2529
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:23:59 +0530

    add f1 0x420 device

---
+++ sdk/core/flash/stm32/core/internal/DevF1x420.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF1x420 extends Device {
+
+    public DevF1x420() {
+        pid = 0x420;
+        mcuSeries = "STM32F10xxx";
+        RAMMemStartAddr = 0x20000200;
+        RAMMemEndAddr = 0x20001FFF;
+        IBSysMemStartAddr = 0x1FFFF000;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
