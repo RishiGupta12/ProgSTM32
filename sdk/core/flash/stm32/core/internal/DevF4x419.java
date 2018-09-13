af665a089081e31b4f7e6247a4d213ea7a249a46
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:50:35 +0530

    add f4 series 0x419 device

---
+++ sdk/core/flash/stm32/core/internal/DevF4x419.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF4x419 extends Device {
+
+    public DevF4x419() {
+        pid = 0x419;
+        mcuSeries = "STM32F42xxx/43xxx";
+        RAMMemStartAddr = 0x20003000;
+        RAMMemEndAddr = 0x2002FFFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF77FF;
+    }
+}
