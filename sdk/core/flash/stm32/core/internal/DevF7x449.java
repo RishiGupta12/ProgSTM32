f715f7d817b3a536d288bb4d058d4c47ef3b15ed
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:11:37 +0530

    add f7 0x449 device

---
+++ sdk/core/flash/stm32/core/internal/DevF7x449.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF7x449 extends Device {
+
+    public DevF7x449() {
+        pid = 0x449;
+        mcuSeries = "STM32F74xxx/75xxx";
+        RAMMemStartAddr = 0x20004000;
+        RAMMemEndAddr = 0x2004FFFF;
+        IBSysMemStartAddr = 0x1FF00000;
+        IBSysMemEndAddr = 0x1FF0EDBF;
+    }
+}
