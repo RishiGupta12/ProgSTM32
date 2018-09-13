27ab465645acf94bec0ddcc16554e748c3b4a7c4
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:36:37 +0530

    add L0 0x447 device

---
+++ sdk/core/flash/stm32/core/internal/DevL0x447.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL0x447 extends Device {
+
+    public DevL0x447() {
+        pid = 0x447;
+        mcuSeries = "STM32L07xxx/08xxx";
+        RAMMemStartAddr = 0x00;
+        RAMMemEndAddr = 0x00;
+        IBSysMemStartAddr = 0x1FF00000;
+        IBSysMemEndAddr = 0x1FF01FFF;
+    }
+}
