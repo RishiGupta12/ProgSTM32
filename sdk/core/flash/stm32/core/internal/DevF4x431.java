635f4229b70bd5f59430e644a69b6752a54c78b9
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:53:45 +0530

    add f4 series 0x4231 device

---
+++ sdk/core/flash/stm32/core/internal/DevF4x431.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF4x431 extends Device {
+
+    public DevF4x431() {
+        pid = 0x431;
+        mcuSeries = "STM32F446xx";
+        RAMMemStartAddr = 0x20001000;
+        RAMMemEndAddr = 0x2000FFFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF77FF;
+    }
+}
