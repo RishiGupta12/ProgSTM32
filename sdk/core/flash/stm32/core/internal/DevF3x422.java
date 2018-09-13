b51d5729215976517601090032d63855487068b5
Rishi Gupta <gupt21@gmail.com>
2018-09-13 12:47:43 +0530

    add stm32f3 system memory addresses

--- sdk/core/flash/stm32/core/internal/DevF3x422.java
+++ sdk/core/flash/stm32/core/internal/DevF3x422.java
@@ -8,26 +8,12 @@ import flash.stm32.core.Device;
 
 final class DevF3x422 extends Device {
 
-    protected int pid;
-    protected String mcuSeries;
-
-    protected int flashMemSize;
-    protected int mainMemStartAddr;
-    protected int mainMemEndAddr;
-
-    protected int numBanks;
-    protected int numPagesInABank;
-    protected int pageSize;
-
-    protected int IBSysMemStartAddr;
-    protected int IBSysMemEndAddr;
-
-    protected int RAMMemStartAddr;
-    protected int RAMMemEndAddr;
-
     public DevF3x422() {
         pid = 0x422;
+        mcuSeries = "STM32F302xB(C)/303xB(C) - STM32F358xx";
         RAMMemStartAddr = 0x20001400;
         RAMMemEndAddr = 0x20009FFF;
+        IBSysMemStartAddr = 0x1FFFD800;
+        IBSysMemEndAddr = 0x1FFFF7FF;
     }
 }
