120b05d7b16871f7f666a3c6719dfde443a742f6
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:32:26 +0530

    add f3 0x446 device

---
+++ sdk/core/flash/stm32/core/internal/DEvF3x446.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF3x446 extends Device {
+
+    public DevF3x446() {
+        pid = 0x446;
+        mcuSeries = "STM32F302xD(E)/303xD(E) - STM32F398xx";
+        RAMMemStartAddr = 0x00;
+        RAMMemEndAddr = 0x00;
+        IBSysMemStartAddr = 0x1FFFD800;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
