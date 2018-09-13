1b82cf2accb65de016bce1d3ce58b94fad65d6c9
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:54:54 +0530

    add f4 series 0x433 device

---
+++ sdk/core/flash/stm32/core/internal/DevF4x433.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF4x433 extends Device {
+
+    public DevF4x433() {
+        pid = 0x433;
+        mcuSeries = "STM32F401xD(E)";
+        RAMMemStartAddr = 0x20001000;
+        RAMMemEndAddr = 0x20017FFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF77FF;
+    }
+}
