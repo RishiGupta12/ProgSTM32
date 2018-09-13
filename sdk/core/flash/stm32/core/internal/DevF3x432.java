be5973730114c8cc33b7e52236f68b1ce63bbb94
Rishi Gupta <gupt21@gmail.com>
2018-09-13 12:54:02 +0530

    add stm32f3 0x432 dev information

---
+++ sdk/core/flash/stm32/core/internal/DevF3x432.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF3x432 extends Device {
+
+    public DevF3x432() {
+        pid = 0x432;
+        mcuSeries = "STM32F373xx - STM32F378xx";
+        RAMMemStartAddr = 0x00;
+        RAMMemEndAddr = 0x00;
+        IBSysMemStartAddr = 0x1FFFD800;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
