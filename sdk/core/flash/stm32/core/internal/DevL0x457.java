fbfdf742e944d3bffcd2f61277a9c0019f0dd92d
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:37:46 +0530

    add L0 0x457 device

---
+++ sdk/core/flash/stm32/core/internal/DevL0x457.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL0x457 extends Device {
+
+    public DevL0x457() {
+        pid = 0x457;
+        mcuSeries = "STM32L01xxx/02xxx";
+        RAMMemStartAddr = 0x00;
+        RAMMemEndAddr = 0x00;
+        IBSysMemStartAddr = 0x1FF00000;
+        IBSysMemEndAddr = 0x1FF00FFF;
+    }
+}
