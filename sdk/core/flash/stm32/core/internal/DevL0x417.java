f1444d0e2d488c6dd59c6b56f03590116b94b929
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:33:45 +0530

    add L0 0x417 device

---
+++ sdk/core/flash/stm32/core/internal/DevL0x417.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL0x417 extends Device {
+
+    public DevL0x417() {
+        pid = 0x417;
+        mcuSeries = "STM32L05xxx/06xxx";
+        RAMMemStartAddr = 0x20001000;
+        RAMMemEndAddr = 0x20001FFF;
+        IBSysMemStartAddr = 0x1FF00000;
+        IBSysMemEndAddr = 0x1FF00FFF;
+    }
+}
