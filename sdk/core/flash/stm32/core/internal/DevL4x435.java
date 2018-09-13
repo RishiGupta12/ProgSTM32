7a2cba0a94c2b4225273012ac9410eb1acf8b2c2
Rishi Gupta <gupt21@gmail.com>
2018-09-13 16:29:18 +0530

    add L4 0x435 device

---
+++ sdk/core/flash/stm32/core/internal/DevL4x435.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL4x435 extends Device {
+
+    public DevL4x435() {
+        pid = 0x435;
+        mcuSeries = "STM32L43xxx/44xxx";
+        RAMMemStartAddr = 0x20003100;
+        RAMMemEndAddr = 0x2000BFFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF6FFF;
+    }
+}
