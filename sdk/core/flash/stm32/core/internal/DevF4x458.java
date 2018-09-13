146a3f061d37a913ec5f7521a67e1ea91b7dda6d
Rishi Gupta <gupt21@gmail.com>
2018-09-13 13:58:19 +0530

    add f4 series 0x458 device

---
+++ sdk/core/flash/stm32/core/internal/DevF4x458.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF4x458 extends Device {
+
+    public DevF4x458() {
+        pid = 0x458;
+        mcuSeries = "STM32F410xx";
+        RAMMemStartAddr = 0x20001000;
+        RAMMemEndAddr = 0x20007FFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF77FF;
+    }
+}
