d37cac28b90e5960352f80c646f9a67f159e6bb3
Rishi Gupta <gupt21@gmail.com>
2018-09-13 07:29:34 +0530

    add f3 pid based device creation

---
+++ sdk/core/flash/stm32/core/internal/DevF05xxxF030x8P440.java
@@ -0,0 +1,11 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF05xxxF030x8P440 extends Device {
+
+}
--- sdk/core/flash/stm32/core/internal/DeviceCreator.java
+++ sdk/core/flash/stm32/core/internal/DeviceCreator.java
@@ -16,6 +16,23 @@ import flash.stm32.core.Device;
 public class DeviceCreator {
 
     public Device createDevFromPID(int pid) {
+
+        if ((pid >= 0x422) && (pid <= 0x446)) {
+            // F3 series
+            switch (pid) {
+            case 0x422:
+                break;
+            case 0x432:
+                break;
+            case 0x438:
+                break;
+            case 0x439:
+                break;
+            case 0x446:
+                break;
+            }
+        }
+
         return null;
     }
 }
