9a634cd254db7818ef4a2ca10600583588d02991
Rishi Gupta <gupt21@gmail.com>
2018-09-13 16:30:52 +0530

    add L4 0x461 device

---
+++ sdk/core/flash/stm32/core/internal/DevL4x461.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevL4x461 extends Device {
+
+    public DevL4x461() {
+        pid = 0x461;
+        mcuSeries = "STM32L496xx/4A6xx";
+        RAMMemStartAddr = 0x20003100;
+        RAMMemEndAddr = 0x2003FFFF;
+        IBSysMemStartAddr = 0x1FFF0000;
+        IBSysMemEndAddr = 0x1FFF6FFF;
+    }
+}
