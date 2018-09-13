490863c412677e5b44260895685c4eadb2c0e49f
Rishi Gupta <gupt21@gmail.com>
2018-09-13 15:21:57 +0530

    add f1 0x412 device

---
+++ sdk/core/flash/stm32/core/internal/DevF1x412.java
@@ -0,0 +1,19 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+import flash.stm32.core.Device;
+
+final class DevF1x412 extends Device {
+
+    public DevF1x412() {
+        pid = 0x412;
+        mcuSeries = "STM32F10xxx";
+        RAMMemStartAddr = 0x20000200;
+        RAMMemEndAddr = 0x200027FF;
+        IBSysMemStartAddr = 0x1FFFF000;
+        IBSysMemEndAddr = 0x1FFFF7FF;
+    }
+}
