ef4123a8a2339b9a118c26343185e92a7c655891
Rishi Gupta <gupt21@gmail.com>
2018-09-18 10:13:22 +0530

    add jar resources

---
+++ sdk/resources/flash/stm32/resources/package-info.java
@@ -0,0 +1,12 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+/**
+ * <p>
+ * Common place to hold all resources (language etc.) used by this sdk.
+ * </p>
+ * 
+ * @author Rishi Gupta
+ */
+package flash.stm32.resources;
