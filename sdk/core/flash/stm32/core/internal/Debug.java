688f9da008a7ae04a93eeec6a7643cf2222d1704
Rishi Gupta <gupt21@gmail.com>
2018-09-18 06:21:02 +0530

    add debug class

---
+++ sdk/core/flash/stm32/core/internal/Debug.java
@@ -0,0 +1,11 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core.internal;
+
+public final class Debug {
+
+    /* Set to true to print bytes received */
+    public static final boolean debug = false;
+}
