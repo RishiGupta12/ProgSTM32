e6065a096b2e0a457c031ab38c272acda2fb7a6a
Rishi Gupta <gupt21@gmail.com>
2018-09-18 07:33:59 +0530

    make calling makefile function readable

--- sdk/core/flash/stm32/core/internal/Debug.java
+++ sdk/core/flash/stm32/core/internal/Debug.java
@@ -7,5 +7,5 @@ package flash.stm32.core.internal;
 public final class Debug {
 
     /* Set to true to print bytes received */
-    public static final boolean debug = true;
+    public static final boolean debug = false;
 }
