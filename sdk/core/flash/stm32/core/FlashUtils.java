02f962db9f2b653da17bd112ff98069911f047f6
Rishi Gupta <gupt21@gmail.com>
2018-09-21 19:00:45 +0530

    add general utility class

---
+++ sdk/core/flash/stm32/core/FlashUtils.java
@@ -0,0 +1,13 @@
+/* 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ */
+
+package flash.stm32.core;
+
+public final class FlashUtils {
+
+    public byte[] hexToBinFwFormat(byte[] buf) {
+
+        return null;
+    }
+}
