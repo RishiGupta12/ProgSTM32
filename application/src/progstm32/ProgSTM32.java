d9b51497e932b9598c1ad2f513a4a807ee396207
Rishi Gupta <gupt21@gmail.com>
2018-10-21 14:56:04 +0530

    add progstm32 application and makefiles

---
+++ application/src/progstm32/ProgSTM32.java
@@ -0,0 +1,38 @@
+/* 
+ * This file is part of progstm32.
+ * 
+ * Copyright (C) 2018, Rishi Gupta. All rights reserved.
+ * 
+ * The progstm32 is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation; either version 2.1 of the License, or 
+ * (at your option) any later version.
+ * 
+ * The progstm32 is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this library; if not, write to the Free Software Foundation,Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package progstm32;
+
+/**
+ * <p>
+ * Entry class to the ProgSTM32 application.
+ * </p>
+ * 
+ * @author Rishi Gupta
+ *
+ */
+public final class ProgSTM32 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
--- build/README.md
+++ build/README.md
@@ -1,2 +1,11 @@
 This folder contains binaries generated when make command is run.
 
+### Application
+- progstm32app.jar  : progstm32 application
+
+### SDK
+- progstm32uart.jar : serial port based progstm32 sdk (implements UART protocol as defined in AN2606 application note)
+
+### External libraries
+- sp-tty.jar        : library from serialpundit project
+- sp-core.jar       : library from serialpundit project
