e7823f6b9fa49c7775887fa3886dd81b264311a5
Rishi Gupta <gupt21@gmail.com>
2019-07-22 22:37:39 +0530

    decouple communication super class from serialpundit

--- sdk/core/flash/stm32/core/CommunicationInterface.java
+++ sdk/core/flash/stm32/core/CommunicationInterface.java
@@ -20,11 +20,6 @@
 
 package flash.stm32.core;
 
-import java.util.concurrent.TimeoutException;
-
-import com.serialpundit.core.SerialComException;
-import com.serialpundit.serial.SerialComManager;
-
 import flash.stm32.core.internal.SystemProperties;
 
 /**
@@ -47,91 +42,4 @@ public abstract class CommunicationInterface {
 
         sysprop = new SystemProperties();
     }
-
-    /**
-     * <p>
-     * Opens and configures serial port as per the given parameters. Although most
-     * of the devices uses even parity, few devices may use no parity, therefore
-     * proper documents should be referred for such devices. This also sets the
-     * minimum timeout to 500 milliseconds for reading data from serial port
-     * </p>
-     * 
-     * <p>
-     * Data in input and output buffers is discarded (clear serial port buffers).
-     * </p>
-     * 
-     * @param port
-     *            serial port (COMxx/ttyXX) through which stm32 is connected to host
-     *            computer
-     * @param baudRate
-     *            rate of signal change used for communication, select as per the
-     *            crystal connected to stm32 board for minimum baud rate error
-     * @param dataBits
-     *            number of data bits a serial frame will contain, set to
-     *            DATABITS.DB_8
-     * @param stopBits
-     *            number of stop bits a serial frame will contain, set to
-     *            STOPBITS.SB_1
-     * @param parity
-     *            parity type for error checking, set to PARITY.P_EVEN
-     * @param flowCtrl
-     *            set to FLOWCONTROL.NONE as default bootloader does not use flow
-     *            control
-     * @throws SerialComException
-     *             if the port is not found, unable to open and configure it
-     */
-    public abstract void open(String port, SerialComManager.BAUDRATE baudRate, SerialComManager.DATABITS dataBits,
-            SerialComManager.STOPBITS stopBits, SerialComManager.PARITY parity, SerialComManager.FLOWCONTROL flowCtrl)
-            throws SerialComException;
-
-    /**
-     * <p>
-     * Closes opened serial port and release resources if any.
-     * </p>
-     * 
-     * @throws SerialComException
-     *             if port can not be closed for some reason
-     */
-    public abstract void close() throws SerialComException;
-
-    /**
-     * <p>
-     * Establish communication with bootloader by sending init sequence (0x7F) and
-     * identifies the product id of it. Based on the pid obtained creates and
-     * instance of Device class which represents connected stm32.
-     * </p>
-     * 
-     * @throws SerialComException
-     *             if an error happens when communicating through serial port
-     * @throws TimeoutException
-     *             when bootloader sends NACK or timeout happens
-     * @return an instance of Device class representing stm32 device
-     */
-    public abstract Device initAndIdentifyDevice() throws SerialComException, TimeoutException;
-
-    /**
-     * <p>
-     * Sets the DTR signal of the host side serial port to the given value.
-     * </p>
-     * 
-     * @param value
-     *            true or false corresponding to the voltage level desired at
-     *            hardware level
-     * @throws SerialComException
-     *             if an error happens when communicating through serial port
-     */
-    public abstract void setDTR(boolean value) throws SerialComException;
-
-    /**
-     * <p>
-     * Sets the RTS signal of the host side serial port to the given value.
-     * </p>
-     * 
-     * @param value
-     *            true or false corresponding to the voltage level desired at
-     *            hardware level
-     * @throws SerialComException
-     *             if an error happens when communicating through serial port
-     */
-    public abstract void setRTS(boolean value) throws SerialComException;
 }
